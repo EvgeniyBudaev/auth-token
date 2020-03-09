d6d58a54cad2f5c6c44afe21b948a7875954e444
EvgeniyBudaev <budaev.e@gmail.com>
2020-03-09 17:27:01 +0300

    валидация форм

--- src/components/signup/signup.js
+++ src/components/signup/signup.js
@@ -4,12 +4,45 @@ import { Link } from "react-router-dom";
 import { URL } from '../routes/urls';
 import Input from '../ui/input';
 
+function validateEmail(email) {
+  var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+
 class SignUp extends Component {
 
   state = {
+    isFormValid: false,
     formControls: {
-      email: {},
-      password: {}
+      email: {
+        value: '',
+        type: 'email',
+        label: 'Email',
+        name: 'email',
+        placeholder: 'Email',
+        errorMessage: 'Введите корректный email!',
+        valid: false,
+        touched: false,
+        validation: {
+          required: true,
+          email: true
+        }
+      },
+      password: {
+        value: '',
+        type: 'password',
+        label: 'Пароль',
+        name: 'password',
+        placeholder: 'Пароль',
+        errorMessage: 'Неверный пароль!',
+        valid: false,
+        touched: false,
+        validation: {
+          required: true,
+          minLength: 6
+        }
+      }
     }
   }
 
@@ -18,6 +51,69 @@ class SignUp extends Component {
   onSubmitHandler = (event) => {
     event.preventDefault()
   };
+
+  validateControl = (value, validation) => {
+    if (!validation) {
+      return true
+    }
+
+    let isValid = true
+
+    if (validation.required) {
+      isValid = value.trim() !== '' && isValid
+    }
+
+    if (validation.email) {
+      isValid = validateEmail(value) && isValid
+    }
+
+    if (validation.minLength) {
+      isValid = value.length >= validation.minLength && isValid
+    }
+
+    return isValid
+  };
+
+  onChangeHandler = (event, controlName) => {
+    console.log(`${controlName}: `, event.target.value)
+    const formControls = { ...this.state.formControls }
+    const control = { ...formControls[controlName] }
+    control.value = event.target.value
+    control.touched = true
+    control.valid = this.validateControl(control.value, control.validation)
+    formControls[controlName] = control
+
+    let isFormValid = true
+
+    Object.keys(formControls).forEach(name => {
+      isFormValid = formControls[name].valid && isFormValid
+    })
+
+    this.setState({
+      formControls, isFormValid
+    })
+  };
+
+  renderInputs = () => {
+    return Object.keys(this.state.formControls).map((controlName, index) => {
+      const control = this.state.formControls[controlName]
+      return (
+        <Input
+          key={controlName + index}
+          type={control.type}
+          value={control.value}
+          valid={control.valid}
+          touched={control.touched}
+          label={control.label}
+          name={control.name}
+          placeholder={control.placeholder}
+          shouldValidate={!!control.validation}
+          errorMessage={control.errorMessage}
+          onChange={event => this.onChangeHandler(event, controlName)} />
+      )
+    })
+  }
+
 
   render() {
     return (
@@ -29,17 +125,16 @@ class SignUp extends Component {
             </div>
             <div className={classes.Signup__content}>
               <div className={classes.Signup__inputs}>
-                <Input label="Email" placeholder="Email" />
-                <Input label="Пароль" placeholder="Пароль" errorMessage="tedt" />
+                {this.renderInputs()}
               </div>
               <div className={classes.Signup__btn}>
                 <Link to={URL.HOME} className={classes.link}>
-                  <button
+                  <input
                     type="primary"
                     value="Зарегистрироваться"
                     onClick={this.registerHandler}
                     disabled={!this.state.isFormValid}
-                  >Зарегистрироваться</button>
+                  ></input>
                 </Link>
               </div>
             </div>
