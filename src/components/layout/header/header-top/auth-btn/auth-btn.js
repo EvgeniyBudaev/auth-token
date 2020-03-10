e514c209095b080de708236a114a4987eac1732d
EvgeniyBudaev <budaev.e@gmail.com>
2020-03-10 07:43:51 +0300

    auth-btn

--- src/components/layout/header/header-top/auth-btn/auth-btn.js
+++ src/components/layout/header/header-top/auth-btn/auth-btn.js
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { URL } from '../../../../routes/urls';
 import * as classes from "./auth-btn.module.css";
+import { connect } from 'react-redux';
 
+class AuthBtn extends Component {
+  render() {
 
-const AuthBtn = props => {
-  return (
+    // const links = [
+    //   links.push({ to: "/logout", label: "Выйти", exact: false })
+    // ];
 
-    <div className={classes.HeaderBtn}>
-      <div className={classes.HeaderBtnSignup}><Link to={URL.SIGNUP} className={classes.Signup}>Регистрация</Link></div>
-      <div className={classes.HeaderBtnLogin}><Link to={URL.LOGIN} className={classes.Login}>Войти</Link></div>
-      <div className={classes.HeaderBtnLogin}><Link to={URL.LOGOUT} className={classes.Login}>Выйти</Link></div>
-    </div>
-
-  );
+    if (this.props.isAuthenticated) { }
+    return (
+      <div className={classes.HeaderBtn}>
+        <div className={classes.HeaderBtnSignup}><Link to={URL.SIGNUP} className={classes.Signup}>Регистрация</Link></div>
+        <div className={classes.HeaderBtnLogin}><Link to={URL.LOGIN} className={classes.Login}>Войти</Link></div>
+        <div className={classes.HeaderBtnLogin}><Link to={URL.LOGOUT} className={classes.Login}>Выйти</Link></div>
+      </div>
+    );
+  }
 };
 
-export default AuthBtn;
+function mapStateToProps(state) {
+  return {
+    isAuthenticated: !!state.auth.token
+  }
+}
+
+export default connect(mapStateToProps)(AuthBtn);
